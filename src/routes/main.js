185220e40e04427a8929305b94aaf7a292b33c23
lassev05 <vestergaardlasse2@gmail.com>
2021-12-28 23:46:32 +0100

    new update

--- src/routes/main.js
+++ src/routes/main.js
@@ -31,16 +31,10 @@ router.get("/", (req, res) => {
 });
 
 router.post("/upload", upload.single("image"), (req, res) => {
-    res.send(
-        `
-        <link rel="stylesheet" href="https://unpkg.com/hypestyle@0.1.5/dist/css/hypestyle.min.css">
-
-        <h1 class="text-center mt-2 font-md">You have successfully uploaded an image!</h1> <br> 
-        <p class="text-center">Use this url to view it: <a class="text-primary-dark-1 c-pointer" target="_blank" href="/images/${req.file.filename}">${req.file.filename}</a></p><br/>
-        <div class="w-full text-center">
-        <a class="btn btn-gray text-white tr-sm font-md bs-lg c-pointer" href="https://imghost.hypll.org/">Upload another image</a>
-        </div>`
-    );
+    res.render("uploaded", {
+        url: domain,
+        image: req.file.filename,
+    });
 });
 
 module.exports = router;
